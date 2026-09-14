58cbeb7961b386d72c2743673b81e4f8090e36ab
Georgii Khoruzhevskii <khoruzhevskiigeorgii@gmail.com>
2026-09-14 18:09:00 +0200

    feat: add evaluation package

---
+++ src/fly_window/evaluation/__init__.py
@@ -0,0 +1,5 @@
+"""Frozen held-out evaluation for Fly Window policies."""
+
+from fly_window.evaluation.run import EvaluationSummary, HELD_OUT_SEEDS, evaluate_policy
+
+__all__ = ["EvaluationSummary", "HELD_OUT_SEEDS", "evaluate_policy"]
